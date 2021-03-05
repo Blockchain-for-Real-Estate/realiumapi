b26b9a78481881d96b6a1568d8d34d425df4c43c
Trevan Reese <trevanreeser41@gmail.com>
2021-03-04 20:35:21 -0700

    fill assetSerializer

--- djangoapi/realiumapi/api/serializers.py
+++ djangoapi/realiumapi/api/serializers.py
@@ -17,3 +17,38 @@ class AssetSerializer(serializers.HyperlinkedModelSerializer):
         model = Asset
         fields = ('assetName', 'assetTypeId', 'opportunityType', 'legalTypeId', 'tokenId', 'parcelId', 'streetAddress',
                     'city', 'state', 'zipCode', 'country', 'acerage', 'llc')
+
+        id = serializers.ReadOnlyField(required=False)
+        assetName = serializers.CharField(required=True)
+        assetTypeId = serializers.ReadOnlyField(required=True)
+        opportunityType = serializers.CharField(
+            required=True
+        )
+        legalTypeId = serializers.IntegerField(
+            required=True
+        )
+        tokenId = serializers.ReadOnlyField(required=True)
+        parcelId = serializers.ReadOnlyField(required=True)
+        streetAddress = serializers.CharField(required=True, max_length=50)
+        city = serializers.CharField(
+            required=True,
+            max_length=256
+        )
+        state = serializers.CharField(
+            required=True,
+            max_length=256
+        )
+        zip_code = serializers.IntegerField(
+            required=True
+        )
+        country = serializers.CharField(
+            required=False,
+            read_only=True,
+            max_length=256
+        )
+        acerage = serializers.FloatField(
+            required=False
+        )
+        llc = serializers.CharField(
+            required=True
+        )
