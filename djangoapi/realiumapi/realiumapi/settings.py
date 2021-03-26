1ef3fc8c9d976b4978fce32f1aa6022db4c1ee18
Danny Guzman <dannygooze93@gmail.com>
2021-03-25 20:45:14 -0600

    Fix settings.py

--- djangoapi/realiumapi/realiumapi/settings.py
+++ djangoapi/realiumapi/realiumapi/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8&@@#g3$0x7v#7v*8342#uahm+hrfn+#ry@07@#!(9f@ajrbrk' 
+SECRET_KEY = '8&@@#g3$0x7v#7v*8342#uahm+hrfn+#ry@07@#!(9f@ajrbrk'
 #SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
