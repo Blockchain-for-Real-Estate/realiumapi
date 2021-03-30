8374e9b4c786c6531a71345c37bd2e4d38fc2aa1
Jason <jason.iowa@gmail.com>
2021-03-30 14:17:02 -0600

    Fix models to do auto increment rather than UUID

--- djangoapi/realiumapi/api/serializers.py
+++ djangoapi/realiumapi/api/serializers.py
@@ -2,25 +2,22 @@ from rest_framework import serializers
 
 from .models import Hero, User, Asset, Transaction
 
-class UUIDModelSerializer(serializers.ModelSerializer):
-    id = serializers.UUIDField()
-
-class HeroSerializer(UUIDModelSerializer):
+class HeroSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hero
         fields = ('name', 'alias')
 
-class UserSerializer(UUIDModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
 
-class AssetSerializer(UUIDModelSerializer):
+class AssetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Asset
         fields = "__all__"
 
-class TransactionSerializer(UUIDModelSerializer):
+class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = "__all__"
