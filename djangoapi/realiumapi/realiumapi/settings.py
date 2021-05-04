464437eb6bf9d9e68dba3c7cdf3b313f00381ba2
Jason <jason.iowa@gmail.com>
2021-05-04 16:04:12 -0600

    alsfkn

--- djangoapi/realiumapi/realiumapi/settings.py
+++ djangoapi/realiumapi/realiumapi/settings.py
@@ -160,5 +160,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = "/"
+STATIC_ROOT = "/realiumapi/djangoapi/realiumapi/static"
 STATIC_URL = '/static/'
