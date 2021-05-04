968079f80665a97ba7ec19f7bb6a5e53ea49def4
Jason <jason.iowa@gmail.com>
2021-05-04 17:21:12 -0600

    it didnt work

--- djangoapi/realiumapi/realiumapi/wsgi.py
+++ djangoapi/realiumapi/realiumapi/wsgi.py
@@ -4,7 +4,8 @@ import sys
 from dotenv import load_dotenv
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
-sys.path.append('/home/ubuntu/realiumapi/djangoapi/realiumapi')
+# sys.path.append('/home/ubuntu/realiumapi/djangoapi/realiumapi')
+sys.path.append('/home/ubuntu/env')
 # os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/projects/realiumapi/djangoapi/realiumapi/egg_cache")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "realiumapi.settings")
 from django.core.wsgi import get_wsgi_application
