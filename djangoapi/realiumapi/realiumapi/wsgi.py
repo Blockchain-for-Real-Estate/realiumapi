e7e51834831cc34778fa33d5064baef5e7c3312f
Jason <jason.iowa@gmail.com>
2021-05-05 12:27:40 -0600

    asdf

--- djangoapi/realiumapi/realiumapi/wsgi.py
+++ djangoapi/realiumapi/realiumapi/wsgi.py
@@ -7,6 +7,6 @@ load_dotenv(dotenv_path)
 # sys.path.append('/home/ubuntu/realiumapi/djangoapi/realiumapi')
 sys.path.append('/home/ubuntu/env')
 # os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/projects/realiumapi/djangoapi/realiumapi/egg_cache")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "realiumapi.djangoapi.realiumapi.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "realiumapi.djangoapi.realiumapi.realiumapi.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
