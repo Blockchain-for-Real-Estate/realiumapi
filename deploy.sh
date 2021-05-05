cdb8fdf11786cc8a5814470306fca423a225ca9b
Jason <jason.iowa@gmail.com>
2021-05-05 17:14:34 -0600

    h

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh     
-sudo git pull origin development
-sudo pip3 install -r requirements.txt
-cd djangoapi/realiumapi
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py collectstatic
-sudo systemctl restart nginx
-sudo systemctl restart gunicorn
