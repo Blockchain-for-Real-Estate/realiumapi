fed9466935f9820b655ef443747489c2e10d21b7
Jason <jason.iowa@gmail.com>
2021-05-05 12:33:24 -0600

    move files up one folder

--- djangoapi/realiumapi/settings.py
+++ djangoapi/realiumapi/settings.py
@@ -160,5 +160,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = "/var/www/realiumapi"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
