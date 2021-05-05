667785eb5b4972f7a7e0da7ae236dd7586df479c
Jason <jason.iowa@gmail.com>
2021-05-05 17:15:47 -0600

    j

--- djangoapi/realiumapi/realiumapi/settings.py
+++ djangoapi/realiumapi/realiumapi/settings.py
@@ -165,4 +165,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
-#this was a change i made :) maybe deploying will work, still working, asd;fja;sldkfja;sdjf;alsdjf;laskdf;aksdf
+#this was a change i made :) maybe deploying will work, still working
