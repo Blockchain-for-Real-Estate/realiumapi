e6c90c21e0bbbdf0c0dfdf76fee8a48c021ebb81
Jason Stewart <jason.iowa@gmail.com>
2021-05-06 02:12:27 -0600

    changes to requirements.txt

--- djangoapi/realiumapi/realiumapi/wsgi.py
+++ djangoapi/realiumapi/realiumapi/wsgi.py
@@ -1,11 +1,25 @@
 import os
-import sys
 
+from django.core.wsgi import get_wsgi_application
 from dotenv import load_dotenv
+
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
-sys.path.append('/opt/bitnami/projects/realiumapi/djangoapi/realiumapi')
-os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/projects/realiumapi/djangoapi/realiumapi/egg_cache")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "realiumapi.settings")
-from django.core.wsgi import get_wsgi_application
+sys.path.append('/home/ubuntu/realiumapi/djangoapi/realiumapi')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realiumapi.settings')
+
 application = get_wsgi_application()
+
+
+# import os
+# import sys
+
+# from dotenv import load_dotenv
+# dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+# load_dotenv(dotenv_path)
+# sys.path.append('/opt/bitnami/projects/realiumapi/djangoapi/realiumapi')
+# os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/projects/realiumapi/djangoapi/realiumapi/egg_cache")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "realiumapi.settings")
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
