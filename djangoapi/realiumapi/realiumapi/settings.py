2a35bc80ede250cbd04511259fab4fae588b6d02
Jason Stewart <jason.iowa@gmail.com>
2021-05-06 02:21:25 -0600

    environment var

--- djangoapi/realiumapi/realiumapi/settings.py
+++ djangoapi/realiumapi/realiumapi/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+import environ
+
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
