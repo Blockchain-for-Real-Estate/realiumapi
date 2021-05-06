398205e892601da0f02b666c0b66e29cb822e710
Jason <jason.iowa@gmail.com>
2021-05-05 23:36:58 -0600

    take env file out

--- djangoapi/realiumapi/realiumapi/wsgi.py
+++ djangoapi/realiumapi/realiumapi/wsgi.py
@@ -1,11 +1,11 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-sys.path.append('/home/ubuntu/realiumapi/djangoapi/realiumapi')
+# dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+# load_dotenv(dotenv_path)
+# sys.path.append('/home/ubuntu/realiumapi/djangoapi/realiumapi')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realiumapi.settings')
 
