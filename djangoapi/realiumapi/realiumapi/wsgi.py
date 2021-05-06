e08286857bbedaaff03828fbc6d285eceae30aef
Jason Stewart <jason.iowa@gmail.com>
2021-05-06 02:38:03 -0600

    environment var

--- djangoapi/realiumapi/realiumapi/wsgi.py
+++ djangoapi/realiumapi/realiumapi/wsgi.py
@@ -3,11 +3,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-print("dotenv_path", dotenv_path)
-load_dotenv(dotenv_path)
-sys.path.append('/home/ubuntu/realiumapi/djangoapi/realiumapi')
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realiumapi.settings')
 
 application = get_wsgi_application()
